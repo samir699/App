3cbf0f84a2f884d59bf08f79206398ee4301c4cd
Sameer599 <sameershaik8167@gmail.com>
2024-06-09 22:06:17 -0700

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Sameer Shaik on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
